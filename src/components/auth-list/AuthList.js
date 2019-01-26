dddc48d0778423c2add1c4a84d17c8b72dd08c60
KirillTK <35868628+KirillTK@users.noreply.github.com>
2019-01-26 15:48:33 +0300

    #4/feature: list of authors with search

--- src/components/auth-list/AuthList.js
+++ src/components/auth-list/AuthList.js
@@ -1,14 +1,75 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import uuid from 'uuid/v4';
 import './AuthList.css';
 
+
 class AuthListComponent extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            authors: this.props.authors.directors,
+        };
+
+        this.classes = this.props.classes;
+        this.findPerson = this.findPerson.bind(this);
+    }
+
+
     render() {
-        return (
-            <div className="AuthList">
-                This is Auth list component
+
+
+        return (<div className={'AuthList'}>
+
+                <section className={"container "}>
+
+                    <div className={"author-search"}><input type="text" className="form-control form-control-lg "
+                                                            placeholder="Search"
+                                                            onChange={this.findPerson}/></div>
+                    <div className={"list"}>
+                        {this.renderList()}
+                    </div>
+                </section>
+
             </div>
-        );
+        )
+    }
+
+
+    findPerson(event) {
+
+        let updatedList = this.state.authors;
+        updatedList = updatedList.filter((item) => item.name.toLowerCase().search(event.target.value.toLowerCase()) !== -1);
+        this.setState({items: updatedList});
+
+        console.log('updatedList', updatedList);
+    }
+
+    renderList() {
+        const items = this.state.items;
+        const parent = [];
+        parent.push(<ul  key={uuid()} className={"list-group"}/>);
+        const childs = [];
+
+
+        if (items) {
+            items.forEach(item => {
+                childs.push(<li key={uuid()} className={"list-group-item"}>{item.name}</li>);
+            });
+        } else {
+            this.state.authors.forEach(item=> {
+                childs.push(<li key={uuid()} className={"list-group-item"}>
+                    {item.name}
+                </li>);
+            })
+        }
+
+        parent.push(childs);
+        return parent;
     }
 }
 
+
 export default AuthListComponent;
+
