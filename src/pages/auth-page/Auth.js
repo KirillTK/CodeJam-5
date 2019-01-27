305f15c812e728d49fcee106fef93ebaa82b608c
KirillTK <35868628+KirillTK@users.noreply.github.com>
2019-01-27 16:50:13 +0300

    #19 style/ change formatting

--- src/pages/auth-page/Auth.js
+++ src/pages/auth-page/Auth.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import TimelineComponent from '../../components/Timeline';
+import GalleryComponent from '../../components/gallery/Gallery';
 import './Auth.css';
 
 class AuthPage extends Component {
@@ -12,18 +13,19 @@ class AuthPage extends Component {
       interface: null
     };
   }
-  
-  componentDidMount () {
-      const pageProps = (this.props.location.pathname).split('/');
-      this.currentLanguage = pageProps[3];
-      this.currentAuthorId = pageProps[2];
 
-      this.loadData(this.currentLanguage);
+  componentDidMount() {
+    const pageProps = (this.props.location.pathname).split('/');
+    console.log('pageProps', pageProps);
+    this.currentLanguage = pageProps[3];
+    this.currentAuthorId = pageProps[2];
+
+    this.loadData(this.currentLanguage);
   }
 
   render() {
-    if (this.state.authors !== null && this.state.interface !== null) {      
-      
+    if (this.state.authors !== null && this.state.interface !== null) {
+
       return (
         <div className="container py-2 Auth">
           <div className="row justify-content-center">
@@ -31,21 +33,22 @@ class AuthPage extends Component {
           </div>
           <div className="row mt-4 justify-content-around">
             <div className="col-12 col-sm-8 col-md-4">
-              <img src={this.currentAuthor.photo} alt="Author" className="img-fluid" />
+              <img src={this.currentAuthor.photo} alt="Author" className="img-fluid"/>
             </div>
             <div className="col-12 col-sm-10 col-md-8">
               <div className="row">
                 <h2>{this.state.interface.biografy}</h2>
-                <TimelineComponent events={this.currentAuthor.bio} />
+                <TimelineComponent events={this.currentAuthor.bio}/>
               </div>
               <div className="row">
                 <h2>{this.state.interface.works}</h2>
-                <TimelineComponent events={this.currentAuthor.works} />
+                <TimelineComponent events={this.currentAuthor.works}/>
               </div>
             </div>
           </div>
-          <div className="row mt-4">
+          <div className="col mt-4">
             <h2>{this.state.interface.photo}</h2>
+            <GalleryComponent photos={this.currentAuthor.gallery}/>
           </div>
           <div className="row mt-4">
             <h2>{this.state.interface.video}</h2>
@@ -53,6 +56,8 @@ class AuthPage extends Component {
           <div className="row mt-4">
             <h2>{this.state.interface.map}</h2>
           </div>
+
+
         </div>
       );
     } else {
@@ -60,7 +65,8 @@ class AuthPage extends Component {
         <div className="App">
           <header className="App-header">
             <div className="progress">
-              <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" />
+              <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
+                   aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"/>
             </div>
           </header>
         </div>
@@ -73,12 +79,12 @@ class AuthPage extends Component {
     this.language = language;
 
     this._asyncRequestForAuthors = this.getAuthorData(language).then(data => {
-      this.currentAuthor = data.directors[this.currentAuthorId];
-      this.setState({ authors: data });
+      this.currentAuthor = data.directors[this.currentAuthorId - 1];
+      this.setState({authors: data});
     });
 
     this._asyncRequestForLanguage = this.getInterface(language).then(data => {
-      this.setState({ interface: data });
+      this.setState({interface: data});
     });
   }
 
