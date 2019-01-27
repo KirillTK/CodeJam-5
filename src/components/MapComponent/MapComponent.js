fd4d3fbcb222d07526ac3a18deb0e6c5319c88b7
KirillTK <35868628+KirillTK@users.noreply.github.com>
2019-01-28 01:49:27 +0300

    #18 feature: add yandex map to project

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { YMaps, Map, Placemark, FullscreenControl  } from 'react-yandex-maps';
+
+
+const mapState = {
+  center: [55.751574, 37.573856],
+  zoom: 10,
+};
+
 
 class MapComponent extends Component {
   render() {
     return (
-      <div className="map">
-        Map Here
-      </div>
-    )
-  }
+      <YMaps>
+        <Map defaultState={mapState} width={500} height={400}>
+          <FullscreenControl />
+          <Placemark geometry={[55.684758, 37.738521]} />
+        </Map>
+      </YMaps>
+    );
+  };
 }
 
 export default MapComponent;
