9dee6ce0462671eb219c4f3d96fb0250d819271c
KirillTK <kirill99.tkachev@yandex.by>
2019-01-28 21:03:50 +0300

    Updates

---
+++ precache-manifest.fdc593b01645e3ef0f8071c3339665c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7944df03429fb277956f2ac4435bed06",
+    "url": "/CodeJam-5/static/media/play.7944df03.svg"
+  },
+  {
+    "revision": "cd8001780f013eb8d0fb",
+    "url": "/CodeJam-5/static/js/runtime~main.cd800178.js"
+  },
+  {
+    "revision": "d2ad93af979382db85b4",
+    "url": "/CodeJam-5/static/js/main.d2ad93af.chunk.js"
+  },
+  {
+    "revision": "52318fb98c51d76b0fb0",
+    "url": "/CodeJam-5/static/js/1.52318fb9.chunk.js"
+  },
+  {
+    "revision": "d2ad93af979382db85b4",
+    "url": "/CodeJam-5/static/css/main.0969937f.chunk.css"
+  },
+  {
+    "revision": "52318fb98c51d76b0fb0",
+    "url": "/CodeJam-5/static/css/1.f1fe2b1e.chunk.css"
+  },
+  {
+    "revision": "d9cd037774554b3dc09f290721fe9df1",
+    "url": "/CodeJam-5/index.html"
+  }
+];
